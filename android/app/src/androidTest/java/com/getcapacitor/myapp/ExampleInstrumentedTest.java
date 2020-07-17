6abd53dd71fd754afbea1bd1d2cfa5017cb506ac
jcesarmobile <jcesarmobile@gmail.com>
2020-07-17 13:04:28 +0200

    Convert to regular Capacitor app

--- android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
@@ -17,11 +17,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+  @Test
+  public void useAppContext() throws Exception {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.getcapacitor.app", appContext.getPackageName());
-    }
+    assertEquals("com.getcapacitor.app", appContext.getPackageName());
+  }
 }
